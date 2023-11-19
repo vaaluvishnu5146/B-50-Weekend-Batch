de17d38f61ea9d021c29af67073e7f4b377fe1c6
vishnu vardhan <vaaluvishnu5146@gmail.com>
2023-11-19 16:29:32 +0530

    Sanitising App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import React from "react";
-import ButtonComponent from "./Components/Buttons/Button";
 import "./App.css";
-import TextField from "./Components/TextField/TextField";
-import { Routes, Route, Link } from "react-router-dom";
-import home from "./pages/home";
-import contact from "./pages/contact";
 
 function App() {
   return <div className="App">Application</div>;
